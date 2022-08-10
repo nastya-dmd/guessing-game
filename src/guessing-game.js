5a91bfa4f90a1a96e789d0bbae9cb625af7a57a1
Anastasiya Demidchik <iskhakova.anastasiya@gmail.com>
2022-08-10 15:50:05 +0200

    feat: add guessing-game code

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { }
+    start = null;
+    end = null;
+    currentNumber = null
 
     setRange(min, max) {
-
+        this.start = min;
+        this.end = max;
     }
 
     guess() {
-
+        this.currentNumber = Math.round((this.start + this.end) / 2)
+        return this.currentNumber;
     }
 
     lower() {
-
+        this.end = this.currentNumber
     }
 
     greater() {
-
+        this.start = this.currentNumber
     }
 }
 
 module.exports = GuessingGame;
+
+
